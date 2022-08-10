08fbfddba3e88392b8d857249a2d284f34323f6c
Karl Moran <karlmoran06@gmail.com>
2022-08-10 11:49:11 +0000

    Added ConditionalRenderingClass & Functional

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
-import EventsClass from "./components/EventsClass";
-import EventsFunctional from "./components/EventsFunctional";
-import EventsBinding from "./components/EventsBinding";
+// import EventsClass from "./components/EventsClass";
+// import EventsFunctional from "./components/EventsFunctional";
+// import EventsBinding from "./components/EventsBinding";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
 
 function App() {
   return <div className="App">
       {/* <EventsFunctional />
       <EventsClass /> */}
-      <EventsBinding />
+      {/* <EventsBinding /> */}
+      <ConditionalRenderingFunctional connected={true}/>
+      <ConditionalRenderingClass />
   </div>;
 }
 
