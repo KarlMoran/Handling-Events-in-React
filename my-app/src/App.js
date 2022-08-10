d7da0b6f75cfe25ad33633e1490d97984c8f491f
Karl Moran <karlmoran06@gmail.com>
2022-08-10 12:49:51 +0000

    Nesting Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 // import EventsClass from "./components/EventsClass";
 // import EventsFunctional from "./components/EventsFunctional";
 // import EventsBinding from "./components/EventsBinding";
-import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
-import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+// import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+// import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import NestingComponents from "./components/NestingComponents";
 
 function App() {
   return <div className="App">
       {/* <EventsFunctional />
       <EventsClass /> */}
       {/* <EventsBinding /> */}
-      <ConditionalRenderingFunctional connected={true}/>
-      <ConditionalRenderingClass />
+      {/* <ConditionalRenderingFunctional connected={true}/>
+      <ConditionalRenderingClass /> */}
+      <NestingComponents />
   </div>;
 }
 
